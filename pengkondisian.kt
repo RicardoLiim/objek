19420c5a522efa92d3d98d3a6ff35e00d23d733c
RicardoLiim <97576065+RicardoLiim@users.noreply.github.com>
2022-01-18 10:27:53 +0700

    Add files via upload

---
+++ pengkondisian.kt
@@ -0,0 +1,23 @@
+fun main()
+{
+var a = 100
+
+if (a<=50)
+    {
+        println("Error")
+    } else if (a <= 65) {
+        println("GRADE C");
+    } else if (a <= 70) {
+        println("GRADE B-");
+    } else if (a <= 80) {
+        println("GRADE B");
+    } else if (a <= 90) {
+        println("GRADE A-");
+    } else if (a <= 100) {
+        println("GRADE A");
+
+
+  }
+}
+
+
